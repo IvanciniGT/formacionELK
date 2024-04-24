72db68024291bf2025fd9eae42c87f3622500b22
Ubuntu <ubuntu@ip-172-31-34-165.eu-west-1.compute.internal>
2024-04-24 08:08:06 +0000

    instalacion guay

---
+++ instalaciones/ek/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p /home/ubuntu/environment/datos/master1
+mkdir -p /home/ubuntu/environment/datos/master2
+mkdir -p /home/ubuntu/environment/datos/data1
+mkdir -p /home/ubuntu/environment/datos/data2
+mkdir -p /home/ubuntu/environment/datos/data3
+mkdir -p /home/ubuntu/environment/datos/coordinator1
+mkdir -p /home/ubuntu/environment/datos/coordinator2
+mkdir -p /home/ubuntu/environment/datos/certificados
+mkdir -p /home/ubuntu/environment/datos/kibana
+
+
+chmod 777 /home/ubuntu/environment/datos/master1
+chmod 777 /home/ubuntu/environment/datos/master2
+chmod 777 /home/ubuntu/environment/datos/data1
+chmod 777 /home/ubuntu/environment/datos/data2
+chmod 777 /home/ubuntu/environment/datos/data3
+chmod 777 /home/ubuntu/environment/datos/coordinator1
+chmod 777 /home/ubuntu/environment/datos/coordinator2
+chmod 777 /home/ubuntu/environment/datos/certificados
+chmod 777 /home/ubuntu/environment/datos/kibana
+
---
+++ notas/dia3.md
@@ -0,0 +1,28 @@
+# 1.5 Gigabites... Clavaos!
+
+MEM_LIMIT=1500000000
+
+1 byte = 8 bits
+1 kb =   1000 bytes
+1 Mb =   1000 kb
+1 Gb =   1000 Mb
+
+Hace 25 años, 
+
+1 byte = 8 bits
+1 kb =   1024 bytes
+1 Mb =   1024 kb
+1 Gb =   1024 Mb
+
+Y hace 25 años... se cambió.
+Hoy en día:
+1 byte = 8 bits
+1 kb =   1000 bytes
+1 Mb =   1000 kb
+1 Gb =   1000 Mb
+
+Y lo que ha ocurrido es que han creado una nueva unidad de medida:
+1 byte = 8 bits
+1 kib =   1024 bytes
+1 Mib =   1024 kib
+1 Gib =   1024 Mib
